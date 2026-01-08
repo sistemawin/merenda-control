b00c7ae1e8dec1b6762a327fa568d07f63c6c3f1
henrique moura <carlosmoura32811@gmail.com>
2026-01-07 22:29:14 -0300

    fix: proper redirect from index to login using next/router

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
-export async function getServerSideProps() {
-  return {
-    redirect: {
-      destination: "/login",
-      permanent: false,
-    },
-  };
-}
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
   return null;
 }
